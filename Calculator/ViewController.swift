43743bb07b5a062f54b3985c624e679b57b747df
dino buljubasic <dbnex14@gmail.com>
2020-02-11 14:52:29 -0800

    Add logic to be able to enter multiple numbers into calculator.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,20 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
-    
-    
+    var isFinishedTypingNumber: Bool = true;
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a number is entered into the keypad
-    
+        //To figure out button, we could use tag values on each one.
+        //But that is bit manual and I want to know identity of a
+        //tapped button and that can also be retrieved from sender.
+        // Since currentTitle is optional, we use if let
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
 
 }
